c01876a7a91be288f945fc915fb19cf6ed48f8af
ruth Thomas <RuthThomas@MacBook-Pro-9.local>
2025-02-04 19:57:09 +1000

    Fixed duplicate dotenv import

--- bronson-paws.js
+++ bronson-paws.js
@@ -3,9 +3,6 @@ import OpenAI from "openai";
 import dotenv from 'dotenv';
 dotenv.config();
 
-import dotenv from 'dotenv';
-dotenv.config();
-
 const openai = new OpenAI({
     apiKey: process.env.OPENAI_API_KEY
 });
